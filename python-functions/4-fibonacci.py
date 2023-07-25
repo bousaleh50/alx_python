0f32c12454a037d26232a635e3e7f6292323c0e1
AHMED BOUSALEH <92605896+bousaleh50@users.noreply.github.com>
2023-07-25 20:41:38 +0100

    update the fibonnaci function

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,8 +1,16 @@
 def fibonacci_sequence(n):
+   if(n==1):
+       return [1]
+   elif(n==0):
+       return []
    sequence=[0,1]
    for i in range(2,n):
        sequence.append(sequence[i-1]+sequence[i-2])
 
    return sequence
 
+print(fibonacci_sequence(6))
+print(fibonacci_sequence(1))
+print(fibonacci_sequence(0))
+print(fibonacci_sequence(20))
 
