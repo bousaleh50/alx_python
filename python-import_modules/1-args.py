05d116f0a0da97442c54b961c215d4acd87963c9
AHMED BOUSALEH <92605896+bousaleh50@users.noreply.github.com>
2023-07-28 16:13:39 +0100

    update the file 1-args.py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,13 +1,13 @@
 if __name__=="__main__":
     import sys
     
-    if(len(sys.argv)==0):
+    if(len(sys.argv)-1==0):
         print("0 arguments.")
     else:
-        if(len(sys.argv)==1):
+        if(len(sys.argv)-1==1):
             print("1 argument:")
         else:
-            print(f"{len(sys.argv)} arguments:")
+            print(f"{len(sys.argv)-1} arguments:")
             for i in range(1,len(sys.argv)):
                 print("{}: {}".format(i,sys.argv[i]))
         
