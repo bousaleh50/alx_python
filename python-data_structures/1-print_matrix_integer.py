346b755841ef5cb82a636daad9790c1ac2a5b6ca
AHMED BOUSALEH <92605896+bousaleh50@users.noreply.github.com>
2023-07-29 19:39:28 +0100

    add file print-matrix

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,14 @@
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]),end="_"if j!=len(matrix[i])-1 else "")
+        print("$")
+    print("--$")
+    
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
