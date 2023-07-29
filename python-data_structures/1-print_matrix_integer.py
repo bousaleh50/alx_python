210ffa76cdb25e5db69eea4d641c5ed4ae952d90
AHMED BOUSALEH <92605896+bousaleh50@users.noreply.github.com>
2023-07-29 19:44:58 +0100

    add file print-matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,9 +1,8 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]),end="_"if j!=len(matrix[i])-1 else "")
-        print("$")
-    print("--$")
+            print("{:d} ".format(matrix[i][j]),end="")
+        print("")
     
 matrix = [
     [1, 2, 3],
