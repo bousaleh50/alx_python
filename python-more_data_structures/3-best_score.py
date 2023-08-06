e1732ad702768182e43b38bd09c15c5fa02b1269
AHMED BOUSALEH <92605896+bousaleh50@users.noreply.github.com>
2023-08-06 20:32:31 +0100

    update the file 3-best_scire

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,5 +1,4 @@
-def best_score(a_dictionar):
-    if a_dictionar==None or len(a_dictionar)==0:
+def best_score(a_dictionary):
+    if not a_dictionary:
         return None
-    return max(a_dictionar)
-
+    return max(a_dictionary, key=a_dictionary.get)
