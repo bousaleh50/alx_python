e98c2fff9ba91b5395f183084fd7b0596d0f2339
AHMED BOUSALEH <92605896+bousaleh50@users.noreply.github.com>
2023-08-06 20:18:44 +0100

    add the ile 4-multiply-list_map.py

--- python-more_data_structures/4-multiply_list_map.py
+++ python-more_data_structures/4-multiply_list_map.py
@@ -1,4 +1,6 @@
 def multiply_list_map(my_list=[],number=0):
-    new_list=list(map(lambda x:x*number,my_list))
+    numbers=map(lambda x:x*number,my_list)
+    new_list=list(numbers)
     return new_list
+
 
