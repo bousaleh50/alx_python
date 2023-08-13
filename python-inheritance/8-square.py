f1dfb063f9536b8d48d24f11e4fec7f8807d2e88
AHMED BOUSALEH <92605896+bousaleh50@users.noreply.github.com>
2023-08-13 19:26:49 +0100

    documented the file

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -20,10 +20,20 @@ class Square(Rectangle):
         Rectangle (_type_): _description_
     """
     def __init__(self, size):
+        """_summary_
+
+        Args:
+            size (_type_): _description_
+        """
         self.__size = 0
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
     def __str__(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
         return f"[Square] {self.__size}/{self.__size}"
