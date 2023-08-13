a42e1b02b805a58298264682950be657bf495a29
AHMED BOUSALEH <92605896+bousaleh50@users.noreply.github.com>
2023-08-13 19:11:16 +0100

    update the file 8-square

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -8,37 +8,9 @@
     Returns:
         _type_: _description_
     """
-class BaseGeometry:
-    """_summary_
-    """
-    def area(self):
-        raise Exception("area() is not implemented")
 
-    def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+Rectangle=__import__("7-rectangle.py").Rectangle
 
-class Rectangle(BaseGeometry):
-    """_summary_
-
-    Args:
-        BaseGeometry (_type_): _description_
-    """
-    def __init__(self, width, height):
-        self.__width = 0
-        self.__height = 0
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        return self.__width * self.__height
-
-    def __str__(self):
-        return f"[Rectangle] {self.__width}/{self.__height}"
 
 class Square(Rectangle):
     """_summary_
