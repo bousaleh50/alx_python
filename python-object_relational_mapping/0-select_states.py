1ab55b12776904c418c749b61fbccd7d551d4f35
AHMED BOUSALEH <92605896+bousaleh50@users.noreply.github.com>
2023-08-27 19:56:06 +0100

    putting it all together

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -23,4 +23,4 @@ if __name__ == '__main__':
 
     for row in rows_selected:
         print(row)
-        
+     
--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -28,4 +28,4 @@ if __name__ == "__main__":
 
     for row in rows_selected:
         print(row)
-                        
+                   
