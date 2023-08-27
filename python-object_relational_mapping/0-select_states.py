97483a75d8160598a4049417716f661ab66636ef
AHMED BOUSALEH <92605896+bousaleh50@users.noreply.github.com>
2023-08-27 18:21:03 +0100

    connect to db and listst all states

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+"""
+ this script lists all states from the database 
+"""
+import MySQLdb
+from sys import argv
+
+if __name__=="__main__":
+    """
+    access to the database and get the statesfrom the db
+
+    """
+
+    db_connect=MySQLdb.connect(host="localhost",user=argv[1],port=3306,passwd=argv[2],db=argv[3])
+    db_cursor=db_connect.cursor()
+    
+    db_cursor.execute("SELECT * FROM states")
+    
+    states=db_cursor.fetchall()
+    
+    for s in states:
+        print(s)
+        
+    
