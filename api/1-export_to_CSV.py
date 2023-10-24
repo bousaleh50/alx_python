571977da9c301a00b787c44d7020614d75a32472
ahmed <bousalehahmed5@gmail.com>
2023-10-24 17:22:51 +0200

    testing commit

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -50,5 +50,5 @@ def get_employee_todo_progress(employee_id):
             csv_writer.writerow([employee_id, employee_data['username'], task['completed'], task['title']])
 
 if __name__ == "__main__":
-    employee_id = sys.argv[1]
+    employee_id = int(sys.argv[1])
     get_employee_todo_progress(employee_id)
